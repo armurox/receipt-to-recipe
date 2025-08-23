524e443958231d048cb91af824d1d78ba599b157
0xecho <elias.amhat@gmail.com>
2025-08-23 12:51:12 +0400

    Create settings API endpoint

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ReceiptViewSet, IngredientViewSet, RecipeView
+from .views import ReceiptViewSet, IngredientViewSet, RecipeView, SettingsView
 
 router = DefaultRouter()
 router.register(r'receipts', ReceiptViewSet, basename='receipt')
@@ -9,4 +9,5 @@ router.register(r'ingredients', IngredientViewSet, basename='ingredient')
 urlpatterns = [
     path('', include(router.urls)),
     path('recipes/', RecipeView.as_view(), name='recipes-by-ingredients'),
+    path('settings/', SettingsView.as_view(), name='settings'),
 ]
